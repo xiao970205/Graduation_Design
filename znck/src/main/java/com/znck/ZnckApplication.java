3ab1ff99c0dd58b382d8a342fe9966bffe762a27
肖舒翔 <1037426886@qq.com>
2019-01-18 10:48:57 +0800

    添加前端成功，基本框架搭建完成。

--- znck/src/main/java/com/znck/ZnckApplication.java
+++ znck/src/main/java/com/znck/ZnckApplication.java
@@ -3,11 +3,7 @@ package com.znck;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.znck.service.RunService;
 
 @SpringBootApplication
 @MapperScan("com.znck.mapper")
